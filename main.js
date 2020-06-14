dee9d40f20912cf9ceb90c18d941d8d4d633a5e5
Jing-Chen <jcpeng2@illinois.edu>
2020-06-14 02:25:47 -0500

    Playable game

--- main.js
+++ main.js
@@ -1,66 +1,158 @@
 let windowHeight = $(window).height();
 let windowWidth = $(window).width();
-console.log(windowHeight + ", " + windowWidth);
+// console.log(windowHeight + ", " + windowWidth);
 
 let imageHeightPx = 400;
 let scaling = windowHeight / imageHeightPx;
 let imageWidthPx = windowWidth / scaling;
 
 let imagePlayerWidthPx = 7;
-let playerWidthPx = imagePlayerWidthPx * scaling;
-let playerSideBuffer = playerWidthPx;
+let imagePlayerHeightPx = 14;
+let playerSideBuffer = imagePlayerWidthPx*5;
 
 function resize_handle() {
-    windowHeight = $(window).height();
-    windowWidth = $(window).width();
-    console.log(windowHeight + ", " + windowWidth);
+    windowHeight = scrolling_background.getBoundingClientRect().height;
+    windowWidth = scrolling_background.getBoundingClientRect().width;
+    // console.log(windowHeight + ", " + windowWidth);
 
     scaling = windowHeight / imageHeightPx;
     imageWidthPx = windowWidth / scaling;
-    playerWidthPx = imagePlayerWidthPx * scaling;
-    playerSideBuffer = playerWidthPx;
+    // console.log(scaling)
+    updatePlayerOnScreen();
 }
 
 let moving = false;
 let scrolling_background = document.getElementById('scrollables');
 function scroll_handle() {
-    if (!moving) {
-        console.log(scrolling_background.scrollLeft)
+    if (!moving && Math.abs(anticipated_scroll - scrolling_background.scrollLeft) > 1) {
+        // console.log("automove");
+        leftBorderX = scrolling_background.scrollLeft / scaling;
+        if (playerX - leftBorderX < playerSideBuffer) {
+            /* Within playerSideBuffer of left border. */
+            playerAutoMove = true;
+            playerTargetX = leftBorderX + 2*playerSideBuffer;
+        }
+        else if (leftBorderX + imageWidthPx - playerX - imagePlayerWidthPx < playerSideBuffer) {
+            /* Within playerSideBuffer of right border. imageWidthPx is width of screen in image pixels. */
+            playerAutoMove = true;
+            playerTargetX = leftBorderX + imageWidthPx - 2*playerSideBuffer - imagePlayerWidthPx;
+        }
     }
+    updatePlayerOnScreen();
 }
 
-let playerX = 0;
-let playerY = 0;
-let playerY = 0;
-let scrollX = 0;
+let player_box = document.getElementById('player');
+
+// console.log(player_box.style.zIndex);
+
+let playerX = imageWidthPx - playerSideBuffer - imagePlayerWidthPx;
+let playerY = 370;
+let anticipated_scroll = 0;
+let leftBorderX = 0;
 
 let arrowUp = false;
 let arrowDown = false;
 let arrowLeft = false;
 let arrowRight = false;
+let playerAutoMove = false;
+let playerTargetX = 0;
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === "ArrowUp")         arrowUp = true;
-    else if (e.code === "ArrowDown")  arrowDown = true;
-    else if (e.code === "ArrowLeft")  arrowLeft = true;
-    else if (e.code === "ArrowRight") arrowRight = true;
-
-    // scrolling_background.scrollTo(playerSpriteX, 0);
-    // console.log(playerSpriteX);
+window.addEventListener('keydown', (e) => {
+    let captured = false;
+    if (e.code === "ArrowUp") {
+        arrowUp = true;
+        captured = true;
+    }
+    else if (e.code === "ArrowDown") {
+        arrowDown = true;
+        captured = true;
+    }
+    else if (e.code === "ArrowLeft") {
+        arrowLeft = true;
+        captured = true;
+    }
+    else if (e.code === "ArrowRight") {
+        arrowRight = true;
+        captured = true;
+    }
+    
+    if (captured) e.preventDefault();
 });
 
-document.addEventListener('keyup', (e) => {
+window.addEventListener('keyup', (e) => {
     if (e.code === "ArrowUp")         arrowUp = false;
     else if (e.code === "ArrowDown")  arrowDown = false;
     else if (e.code === "ArrowLeft")  arrowLeft = false;
     else if (e.code === "ArrowRight") arrowRight = false;
-
-    scrolling_background.scrollTo(playerSpriteX, 0);
 });
+
+function playerOnScreen() {
+    //return playerX > leftBorderX - imagePlayerWidthPx && playerX < leftBorderX + imageWidthPx;
+    /* It's not exact on the right hand side because of automove... */
+    return playerX > leftBorderX - imagePlayerWidthPx && playerX < leftBorderX + imageWidthPx - imagePlayerWidthPx;
+}
 
 function loop() {
     moving = arrowUp || arrowDown || arrowLeft || arrowRight;
-    if (arrowUp) 
+    if (arrowUp)    playerY -= 2;
+    if (arrowDown)  playerY += 2;
+    if (arrowLeft)  playerX -= 5;
+    if (arrowRight) playerX += 5;
+
+    if (moving) {
+        playerAutoMove = false;
+
+        if (playerX - leftBorderX < playerSideBuffer) {
+            /* Within playerSideBuffer of left border. */
+            leftBorderX = playerX - playerSideBuffer;
+        }
+        else if (leftBorderX + imageWidthPx - playerX - imagePlayerWidthPx < playerSideBuffer) {
+            /* Within playerSideBuffer of right border. imageWidthPx is width of screen in image pixels. */
+            leftBorderX = playerX + playerSideBuffer - imageWidthPx + imagePlayerWidthPx;
+        }
+    }
+
+    if (playerAutoMove) {
+        let playerMoveAmount = (playerTargetX - playerX) * 0.3;
+        let speedCap = 15;
+        if (playerOnScreen()) {
+            speedCap = 5;
+        }
+        if (playerMoveAmount < -speedCap) playerMoveAmount = -speedCap;
+        if (playerMoveAmount > speedCap) playerMoveAmount = speedCap;
+        playerX += playerMoveAmount;
+        if (Math.abs(playerMoveAmount) < 1) {
+            playerAutoMove = false;
+        }
+    }
+    if (playerX < 0) playerX = 0;
+    if (playerX + imagePlayerWidthPx > 2399.9) playerX = 2399.9 - imagePlayerWidthPx;
+    
+    if (playerY < 355 - imagePlayerHeightPx) playerY = 355 - imagePlayerHeightPx;
+    if (playerY > 395 - imagePlayerHeightPx) playerY = 395 - imagePlayerHeightPx;
+
+    /*
+     * Smooth scrolling.
+     */
+    let scrollTarget = leftBorderX * scaling;
+    let scrollCurrent = scrolling_background.scrollLeft;
+    let scrollDelta = (scrollTarget - scrollCurrent) * 0.3;
+    anticipated_scroll = scrollCurrent + scrollDelta;
+    scrolling_background.scrollBy(scrollDelta, 0);
+    // console.log(playerX)
+    updatePlayerOnScreen();
 }
 
-setInterval(loop, 50);
+function updatePlayerOnScreen() {
+    player_box.style.left = ((playerX * scaling) - scrolling_background.scrollLeft) + 'px';
+    player_box.style.top = (playerY * scaling) + 'px';
+    if (playerOnScreen()) {
+        player_box.style.display = "block";
+    }
+    else {
+        player_box.style.display = "none";
+    }
+}
+
+setInterval(loop, 30);
+resize_handle();
